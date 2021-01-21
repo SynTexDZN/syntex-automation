24b16da78938d635af83b7b457ade037b1fc775d
SynTex <syntexdzn@t-online.de>
2021-01-21 09:57:31 +0100

    Added All Characteristics

--- automation.js
+++ automation.js
@@ -57,6 +57,8 @@ module.exports = class Automation
 
 					resolve(true);
 
+					this.parseAutomation();
+
 					logger.log('success', 'bridge', 'Bridge', '%automation_load_success%!');
 				}
 
@@ -65,12 +67,101 @@ module.exports = class Automation
 		});
 	}
 
-	runAutomation(id, letters, value)
+	parseAutomation()
+	{
+		try
+		{
+			for(const i in this.automation)
+			{
+				if(this.automation[i].trigger != null)
+				{
+					for(const j in this.automation[i].trigger)
+					{
+						if(this.automation[i].trigger[j].value != null)
+						{
+							this.automation[i].trigger[j].value = JSON.parse(this.automation[i].trigger[j].value);
+						}
+
+						if(this.automation[i].trigger[j].hue != null)
+						{
+							this.automation[i].trigger[j].hue = JSON.parse(this.automation[i].trigger[j].hue);
+						}
+
+						if(this.automation[i].trigger[j].saturation != null)
+						{
+							this.automation[i].trigger[j].saturation = JSON.parse(this.automation[i].trigger[j].saturation);
+						}
+
+						if(this.automation[i].trigger[j].brightness != null)
+						{
+							this.automation[i].trigger[j].brightness = JSON.parse(this.automation[i].trigger[j].brightness);
+						}
+					}
+				}
+
+				if(this.automation[i].condition != null)
+				{
+					for(const j in this.automation[i].condition)
+					{
+						if(this.automation[i].condition[j].value != null)
+						{
+							this.automation[i].condition[j].value = JSON.parse(this.automation[i].condition[j].value);
+						}
+
+						if(this.automation[i].condition[j].hue != null)
+						{
+							this.automation[i].condition[j].hue = JSON.parse(this.automation[i].condition[j].hue);
+						}
+
+						if(this.automation[i].condition[j].saturation != null)
+						{
+							this.automation[i].condition[j].saturation = JSON.parse(this.automation[i].condition[j].saturation);
+						}
+
+						if(this.automation[i].condition[j].brightness != null)
+						{
+							this.automation[i].condition[j].brightness = JSON.parse(this.automation[i].condition[j].brightness);
+						}
+					}
+				}
+
+				if(this.automation[i].result != null)
+				{
+					for(const j in this.automation[i].result)
+					{
+						if(this.automation[i].result[j].value != null)
+						{
+							this.automation[i].result[j].value = JSON.parse(this.automation[i].result[j].value);
+						}
+
+						if(this.automation[i].result[j].hue != null)
+						{
+							this.automation[i].result[j].hue = JSON.parse(this.automation[i].result[j].hue);
+						}
+
+						if(this.automation[i].result[j].saturation != null)
+						{
+							this.automation[i].result[j].saturation = JSON.parse(this.automation[i].result[j].saturation);
+						}
+
+						if(this.automation[i].result[j].brightness != null)
+						{
+							this.automation[i].result[j].brightness = JSON.parse(this.automation[i].result[j].brightness);
+						}
+					}
+				}
+			}
+		}
+		catch(error)
+		{
+			this.logger.log('error', 'bridge', 'Bridge', 'Automation %json_parse_error%! ' + error);
+		}
+	}
+
+	runAutomation(id, letters, values)
 	{
 		if(ready)
 		{
-			value = value.toString();
-			
 			for(var i = 0; i < this.automation.length; i++)
 			{
 				if(eventLock.includes(this.automation[i].id))
@@ -81,25 +172,43 @@ module.exports = class Automation
 						{
 							var index = eventLock.indexOf(this.automation[i].id);
 
-							if(this.automation[i].trigger[j].operation == '>' && parseFloat(value) < parseFloat(this.automation[i].trigger[j].value) && negativeFired.includes(this.automation[i].trigger[j].id))
+							if(this.automation[i].trigger[j].operation == '>' && negativeFired.includes(this.automation[i].trigger[j].id))
 							{
-								eventLock.splice(index, 1);
+								if(values.value != null && this.automation[i].trigger[j].value != null && values.value < this.automation[i].trigger[j].value
+								|| values.hue != null && this.automation[i].trigger[j].hue != null && values.hue < this.automation[i].trigger[j].hue
+								|| values.saturation != null && this.automation[i].trigger[j].saturation != null && values.saturation < this.automation[i].trigger[j].saturation
+								|| values.brightness != null && this.automation[i].trigger[j].brightness != null && values.brightness < this.automation[i].trigger[j].brightness)
+								{
+									eventLock.splice(index, 1);
 
-								logger.debug('Automation [' + this.automation[i].name + '] Unterschritten ' + this.automation[i].id);
+									logger.debug('Automation [' + this.automation[i].name + '] Unterschritten ' + this.automation[i].id);
+								}
 							}
 
-							if(this.automation[i].trigger[j].operation == '<' && parseFloat(value) > parseFloat(this.automation[i].trigger[j].value) && positiveFired.includes(this.automation[i].trigger[j].id))
+							if(this.automation[i].trigger[j].operation == '<' && positiveFired.includes(this.automation[i].trigger[j].id))
 							{
-								eventLock.splice(index, 1);
+								if(values.value != null && this.automation[i].trigger[j].value != null && values.value > this.automation[i].trigger[j].value
+								|| values.hue != null && this.automation[i].trigger[j].hue != null && values.hue > this.automation[i].trigger[j].hue
+								|| values.saturation != null && this.automation[i].trigger[j].saturation != null && values.saturation > this.automation[i].trigger[j].saturation
+								|| values.brightness != null && this.automation[i].trigger[j].brightness != null && values.brightness > this.automation[i].trigger[j].brightness)
+								{
+									eventLock.splice(index, 1);
 
-								logger.debug('Automation [' + this.automation[i].name + '] Überschritten ' + this.automation[i].id);
+									logger.debug('Automation [' + this.automation[i].name + '] Überschritten ' + this.automation[i].id);
+								}
 							}
 
-							if(this.automation[i].trigger[j].operation == '=' && value != this.automation[i].trigger[j].value)
+							if(this.automation[i].trigger[j].operation == '=')
 							{
-								eventLock.splice(index, 1);
+								if(values.value != null && this.automation[i].trigger[j].value != null && values.value != this.automation[i].trigger[j].value
+								|| values.hue != null && this.automation[i].trigger[j].hue != null && values.hue != this.automation[i].trigger[j].hue
+								|| values.saturation != null && this.automation[i].trigger[j].saturation != null && values.saturation != this.automation[i].trigger[j].saturation
+								|| values.brightness != null && this.automation[i].trigger[j].brightness != null && values.brightness != this.automation[i].trigger[j].brightness)
+								{
+									eventLock.splice(index, 1);
 
-								logger.debug('Automation [' + this.automation[i].name + '] Ungleich ' + this.automation[i].id);
+									logger.debug('Automation [' + this.automation[i].name + '] Ungleich ' + this.automation[i].id);
+								}
 							}
 						}
 					}
@@ -118,14 +227,14 @@ module.exports = class Automation
 			{
 				if(this.automation[i].active && !eventLock.includes(this.automation[i].id))
 				{
-					checkTrigger(this.automation[i], id, letters, value.toString());
+					checkTrigger(this.automation[i], id, letters, values);
 				}
 			}
 		}
 	}
 };
 
-async function checkTrigger(automation, id, letters, value)
+async function checkTrigger(automation, id, letters, values)
 {
 	var trigger = null;
 
@@ -133,19 +242,37 @@ async function checkTrigger(automation, id, letters, value)
 	{
 		if(automation.trigger[i].id == id && automation.trigger[i].letters == letters)
 		{
-			if(automation.trigger[i].operation == '>' && parseFloat(value) > parseFloat(automation.trigger[i].value))
+			if(automation.trigger[i].operation == '>')
 			{
-				trigger = automation.trigger[i];
+				if(values.value != null && automation.trigger[i].value != null && values.value > automation.trigger[i].value
+				|| values.hue != null && automation.trigger[i].hue != null && values.hue > automation.trigger[i].hue
+				|| values.saturation != null && automation.trigger[i].saturation != null && values.saturation > automation.trigger[i].saturation
+				|| values.brightness != null && automation.trigger[i].brightness != null && values.brightness > automation.trigger[i].brightness)
+				{
+					trigger = automation.trigger[i];
+				}
 			}
 
-			if(automation.trigger[i].operation == '<' && parseFloat(value) < parseFloat(automation.trigger[i].value))
+			if(automation.trigger[i].operation == '<')
 			{
-				trigger = automation.trigger[i];
+				if(values.value != null && automation.trigger[i].value != null && values.value < automation.trigger[i].value
+				|| values.hue != null && automation.trigger[i].hue != null && values.hue < automation.trigger[i].hue
+				|| values.saturation != null && automation.trigger[i].saturation != null && values.saturation < automation.trigger[i].saturation
+				|| values.brightness != null && automation.trigger[i].brightness != null && values.brightness < automation.trigger[i].brightness)
+				{
+					trigger = automation.trigger[i];
+				}
 			}
 
-			if(automation.trigger[i].operation == '=' && value == automation.trigger[i].value)
+			if(automation.trigger[i].operation == '=')
 			{
-				trigger = automation.trigger[i];
+				if(values.value == null || automation.trigger[i].value == null || values.value == automation.trigger[i].value
+				&& (values.hue == null || automation.trigger[i].hue == null || values.hue == automation.trigger[i].hue)
+				&& (values.saturation == null || automation.trigger[i].saturation == null || values.saturation == automation.trigger[i].saturation)
+				&& (values.brightness == null || automation.trigger[i].brightness == null || values.brightness == automation.trigger[i].brightness))
+				{
+					trigger = automation.trigger[i];
+				}
 			}
 		}
 	}
@@ -154,7 +281,7 @@ async function checkTrigger(automation, id, letters, value)
 	{
 		logger.debug('Trigger Ausgelöst');
 
-		if(automation.condition && automation.condition.length > 0)
+		if(automation.condition != null && automation.condition.length > 0)
 		{
 			checkCondition(automation, trigger);
 		}
@@ -173,30 +300,46 @@ async function checkCondition(automation, trigger)
 	{
 		//var value = platform.readAccessoryService(automation.condition[i].id, automation.condition[i].letters, true);
 
-		var value = dataManager.readAccessoryService(automation.condition[i].id, automation.condition[i].letters);
+		var values = dataManager.readAccessoryService(automation.condition[i].id, automation.condition[i].letters, true);
 
-		if(value != null)
+		if(values != null)
 		{
-			value = value.toString();
-
-			if(automation.condition[i].operation == '>' && parseFloat(value) > parseFloat(automation.condition[i].value))
+			if(automation.condition[i].operation == '>')
 			{
-				condition++;
+				if(values.state != null && automation.condition[i].value != null && values.state > automation.condition[i].value
+				|| values.hue != null && automation.condition[i].hue != null && values.hue > automation.condition[i].hue
+				|| values.saturation != null && automation.condition[i].saturation != null && values.saturation > automation.condition[i].saturation
+				|| values.brightness != null && automation.condition[i].brightness != null && values.brightness > automation.condition[i].brightness)
+				{
+					condition++;
+				}
 			}
 
-			if(automation.condition[i].operation == '<' && parseFloat(value) < parseFloat(automation.condition[i].value))
+			if(automation.condition[i].operation == '<')
 			{
-				condition++;
+				if(values.state != null && automation.condition[i].value != null && values.state < automation.condition[i].value
+				|| values.hue != null && automation.condition[i].hue != null && values.hue < automation.condition[i].hue
+				|| values.saturation != null && automation.condition[i].saturation != null && values.saturation < automation.condition[i].saturation
+				|| values.brightness != null && automation.condition[i].brightness != null && values.brightness < automation.condition[i].brightness)
+				{
+					condition++;
+				}
 			}
 
-			if(automation.condition[i].operation == '=' && value == automation.condition[i].value)
+			if(automation.condition[i].operation == '=')
 			{
-				condition++;
+				if(values.value == null || automation.trigger[i].value == null || values.value == automation.trigger[i].value
+				&& (values.hue == null || automation.trigger[i].hue == null || values.hue == automation.trigger[i].hue)
+				&& (values.saturation == null || automation.trigger[i].saturation == null || values.saturation == automation.trigger[i].saturation)
+				&& (values.brightness == null || automation.trigger[i].brightness == null || values.brightness == automation.trigger[i].brightness))
+				{
+					condition++;
+				}
 			}
 		}
 	}
 
-	if(condition > 0 && ((!automation.combination || automation.combination == 'ALL') && condition >= automation.condition.length) || automation.condition.combination == 'ONE')
+	if(condition > 0 && ((automation.combination == null || automation.combination == 'ALL') && condition >= automation.condition.length) || automation.condition.combination == 'ONE')
 	{
 		logger.debug('Condition Erfüllt');
 
@@ -210,20 +353,27 @@ async function executeResult(automation, trigger)
 	{
 		var url = '';
 
-		if(automation.result[i].url)
+		if(automation.result[i].url != null)
 		{
 			url = automation.result[i].url;
 		}
 
-		if(automation.result[i].id && automation.result[i].letters && automation.result[i].value && automation.result[i].name)
+		if(automation.result[i].id != null && automation.result[i].letters != null && automation.result[i].value != null && automation.result[i].name != null)
 		{
 			var state = null;
 
-			if((state = TypeManager.validateUpdate(automation.result[i].id, automation.result[i].letters, { value : automation.result[i].value })) != null)
+			var values = {
+				value : automation.result[i].value,
+				hue : automation.result[i].hue,
+				saturation : automation.result[i].saturation,
+				brightness : automation.result[i].brightness
+			};
+
+			if((state = TypeManager.validateUpdate(automation.result[i].id, automation.result[i].letters, values)) != null)
 			{
 				if(TypeManager.letterToType(automation.result[i].letters[0]) == 'statelessswitch')
 				{
-					state.event = parseInt(automation.result[i].value);
+					state.event = state.value;
 					state.value = 0;
 				}
 
@@ -231,26 +381,30 @@ async function executeResult(automation, trigger)
 				{
 					var theRequest = {
 						method : 'GET',
-						url : 'http://' + (automation.result[i].bridge || '127.0.0.1') + ':' + eventManager.RouteManager.getPort(automation.result[i].plugin) + '/devices?id=' + automation.result[i].id + '&type=' + TypeManager.letterToType(automation.result[i].letters[0]) + '&counter=' + automation.result[i].letters[1] + '&value=' + automation.result[i].value,
+						url : 'http://' + (automation.result[i].bridge || '127.0.0.1') + ':' + eventManager.RouteManager.getPort(automation.result[i].plugin) + '/devices?id=' + automation.result[i].id + '&type=' + TypeManager.letterToType(automation.result[i].letters[0]) + '&counter=' + automation.result[i].letters[1] + '&value=' + state.value,
 						timeout : 10000
 					};
+
+					if(state.hue != null)
+					{
+						theRequest.url += '&hue=' + state.hue;
+					}
+
+					if(state.saturation != null)
+					{
+						theRequest.url += '&saturation=' + state.saturation;
+					}
+
+					if(state.brightness != null)
+					{
+						theRequest.url += '&brightness=' + state.brightness;
+					}
 
 					fetchRequest(theRequest, automation.name);
 				}
 				else
 				{
-					var value = automation.result[i].value;
-
-					try
-					{
-						value = JSON.parse(automation.result[i].value)
-					}
-					catch(error)
-					{
-						logger.log('error', 'bridge', 'Bridge', '%json_parse_error%! ( ' + automation.result[i].value + ') ' + error);
-					}
-					
-					eventManager.setOutputStream('SynTexAutomation', { id : automation.result[i].id, letters : automation.result[i].letters }, { value : value });
+					eventManager.setOutputStream('SynTexAutomation', { id : automation.result[i].id, letters : automation.result[i].letters }, state);
 				}
 			}
 			else
@@ -311,12 +465,12 @@ async function executeResult(automation, trigger)
 		{
 			logger.log('error', 'bridge', 'Bridge', 'Automation-Lock.json %update_error%! ' + err);
 		}
-	});
 
-	logger.log('success', trigger.id, trigger.letters, '[' + trigger.name + '] %automation_executed[0]% [' + automation.name + '] %automation_executed[1]%!');
+		logger.log('success', trigger.id, trigger.letters, '[' + trigger.name + '] %automation_executed[0]% [' + automation.name + '] %automation_executed[1]%!');
+	});
 }
 
-function fetchRequest(theRequest, name)
+async function fetchRequest(theRequest, name)
 {
 	request(theRequest, (err, response, body) => {
 
