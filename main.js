19a528ebf1eb881ef02457f2cab2507babbf2973
SynTex <syntexdzn@t-online.de>
2022-09-10 02:33:12 +0200

    Cleaned Up

--- main.js
+++ main.js
@@ -1,39 +1,13 @@
 let LogikEngine = require('./src/automation'), RouteManager = require('./src/route-manager');
 
-const EventEmitter = require('events');
-
-module.exports = class AutomationSystem extends EventEmitter
+module.exports = class AutomationSystem
 {
 	constructor(platform)
 	{
-		super();
-
-		super.setMaxListeners(512);
-    
 		this.pluginName = platform.pluginName;
 		this.logger = platform.logger;
 
 		this.RouteManager = new RouteManager(platform.logger, platform.files, platform.api.user.storagePath());
 		this.LogikEngine = new LogikEngine(platform, this);
     }
-
-    setInputStream(stream, sender, callback)
-	{
-		super.on(stream, (destination, state) => {
-			
-			if(sender.id == destination.id && sender.letters == destination.letters)
-			{
-				callback(state);
-
-				this.logger.debug('<<< ' + stream + ' [' + JSON.stringify(destination) + '] ' + JSON.stringify(state));
-			}
-		});
-	}
-
-	setOutputStream(stream, destination, state)
-	{
-		this.logger.debug('>>> ' + stream + ' [' + JSON.stringify(destination) + '] ' + JSON.stringify(state));
-		
-		super.emit(stream, destination, state);
-	}
 }
